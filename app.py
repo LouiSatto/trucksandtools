3496050000732c2d10d0d4c4cf33d147cdded8e2
Loui Satto <x_sato@hotmail.com>
2020-11-22 17:56:02 +0900

    menu highlights

--- app.py
+++ app.py
@@ -4,4 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', page='home')
+
+@app.route('/workers')
+def workers():
+    return render_template('workers.html', page='workers')
+
+@app.route('/cars')
+def cars():
+    return render_template('cars.html', page='cars')
+
+@app.route('/history')
+def history():
+    return render_template('history.html', page='history')
