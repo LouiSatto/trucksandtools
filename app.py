ec8ef05525554c874cf57edd71ca450fedcfb69a
Loui Satto <x_sato@hotmail.com>
2020-11-27 08:53:20 +0900

    form submit 1

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, request
+
+db = sqlite3.connect('database.db')
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        worker1 = request.form.get("worker1")
+        # db.execute("INSERT INTO transactions (type, userid, share, qty, price) VALUES (?,?,?,?,?)", ("buy", int(session.get("user_id")), share["symbol"], int(request.form.get("shares")), price))
+        return render_template('index.html', page='home', worker1 = worker1)
     return render_template('index.html', page='home')
 
 @app.route('/workers')
